78e9ad524b30d32fa9158653c26059f26c860505
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-03-19 20:53:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import shutil
 import gzip
 import os
-file = [".".join(f.split(".")[:-2]) for f in os.listdir(os.getcwd()) if os.path.isfile(f)]#extract files names without ext
 
+file = [".".join(f.split(".")[:-2]) for f in os.listdir(os.getcwd()) if os.path.isfile(f)]#extract files names without ext
+extention = [".".join(f.split(".")[2:]) for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+
+num=0
 for i in range (len(file)):
 
-    if file[i]== ''  :
-        break
-
-    else :
-
+    if extention[i]=='gz':
+        num = num + 1
+        #if file[i]==''
         with gzip.open(file[i] + ".csv.gz", "rb") as f_in: #select all file with ext .gz
 
             with open(file[i] + ".csv", "wb") as f_out:#select files inside .gz here exemple .csv
@@ -17,4 +18,5 @@ for i in range (len(file)):
                 shutil.copyfileobj(f_in, f_out) #extraction of content .csv
 
         os.remove(file[i] + ".csv.gz") #delete ancient file gz
-print("File Removed , all csv files is ready")
+
+print("You've " + str(num)  + " csv's file ready")
